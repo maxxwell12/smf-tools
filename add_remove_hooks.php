6c3e4d8cfa91c6ff66ebee15efaacb813e50df5d
zilladotexe <hello@jasonclemons.me>
2015-09-21 02:20:36 -0400

    update add_remove_hooks.php

--- add_remove_hooks.php
+++ add_remove_hooks.php
@@ -9,24 +9,32 @@
  * file that was distributed with this source code.
  */
 
+/**
+ * Check to make sure SMF exists in this directory
+ */
 if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
 	require_once(dirname(__FILE__) . '/SSI.php');
 elseif (!defined('SMF'))
 	die('<strong>Error:</strong> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
 
-// Define the hooks
-$hook_functions = array(
-	'integrate_pre_include' => 'Sources/Subs-Downloads.php',
-	'integrate_actions' => 'downloads_createActionHook',
-	'integrate_menu_buttons' => 'downloads_createMenuHook',
-);
+/**
+ * Define the hooks in an array
+ * 
+ * Example:
+ * $hook_functions = array('integrate_actions', 'modification_actions');
+ */
+$hook_functions = array();
 
-// Are we uninstalling?
+/**
+ * This determines whether we're installing or uninstalling
+ */
 if ($context['uninstall'])
     $call = 'remove_integration_function';
 else
     $call = 'add_integration_function';
 
-// Do the deed
+/**
+ * Execute the hook install/uninstall
+ */
 foreach ($hook_functions as $hook => $function)
 	$call($hook, $function);
